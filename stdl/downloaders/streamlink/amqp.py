b0a41163dd6a60d253042ad965fa76e77725ae65
rwiv <zc798546@gmail.com>
2025-01-18 17:22:30 +0900

    feat: update amqp logic

--- stdl/downloaders/streamlink/amqp.py
+++ stdl/downloaders/streamlink/amqp.py
@@ -11,13 +11,17 @@ from stdl.config.env import AmqpConfig
 
 
 class Amqp:
-    def __init__(self, queue: str, conf: AmqpConfig):
-        self.queue = queue
+    def __init__(self, conf: AmqpConfig):
         self.url = f"amqp://{conf.username}:{conf.password}@{conf.host}:{conf.port}"
         self.conn: Optional[AsyncioConnection] = None
         self.ch: Optional[Channel] = None
 
-    async def connect(self) -> AsyncioConnection:
+    async def connect(self) -> tuple[AsyncioConnection, Channel]:
+        self.conn = await self.create_connection()
+        self.ch = await self.create_channel()
+        return self.conn, self.ch
+
+    async def create_connection(self) -> AsyncioConnection:
         future = asyncio.get_event_loop().create_future()
 
         def on_open(conn: AsyncioConnection):
@@ -31,23 +35,28 @@ class Amqp:
             on_open_callback=on_open,
             on_open_error_callback=on_error,
         )
-        self.conn = await future
-        return self.conn
+        return await future
 
     async def create_channel(self) -> Channel:
+        if not self.conn:
+            raise ValueError("Connection not created")
         future = asyncio.get_event_loop().create_future()
 
         def on_open(channel: Channel):
             future.set_result(channel)
 
         self.conn.channel(on_open_callback=on_open)
-        self.ch = await future
-        return self.ch
+        return await future
 
-    async def consume(self, callback: Callable[[Channel, Basic.Deliver, BasicProperties, bytes], None]):
-        self.ch = await self.create_channel()
-        self.ch.queue_declare(queue=self.queue)
-        self.ch.basic_consume(queue=self.queue, on_message_callback=callback)
+    def assert_queue(self, queue: str):
+        if not self.ch:
+            raise ValueError("Channel not created")
+        self.ch.queue_declare(queue=queue, passive=False, durable=False, auto_delete=False, exclusive=False)
+
+    def consume(self, queue: str, callback: Callable[[Channel, Basic.Deliver, BasicProperties, bytes], None]):
+        if not self.ch:
+            raise ValueError("Channel not created")
+        self.ch.basic_consume(queue=queue, on_message_callback=callback)
 
     def close(self):
         self.conn.close()
--- tests/recorder/amqp.py
+++ tests/recorder/amqp.py
@@ -9,31 +9,57 @@ from stdl.config.env import get_env
 from stdl.downloaders.streamlink.amqp import Amqp
 from stdl.utils.env import load_env
 
+load_env("../../dev/.env")
+amqp_conf = get_env().amqp
+queue = "tasks"
+amqp = Amqp(amqp_conf)
 
-@pytest.mark.asyncio
-async def test_amqp():
-    print()
-    load_env("../../dev/.env")
-    amqp_conf = get_env().amqp
 
-    queue = "tasks"
+def on_message(ch: Channel, method: Basic.Deliver, props: BasicProperties, body: bytes):
+    content = json.loads(body.decode("utf-8"))
+    print(content)
+    ch.basic_ack(method.delivery_tag)
 
-    amqp = Amqp(queue, amqp_conf)
-    await amqp.connect()
 
-    def on_message(ch: Channel, method: Basic.Deliver, props: BasicProperties, body: bytes):
-        content = json.loads(body.decode("utf-8"))
-        print(content["message"])
-        print(content["author"])
-        ch.basic_ack(method.delivery_tag)
-    await amqp.consume(on_message)
-
-    ch_w = await amqp.create_channel()
-    ch_w.queue_declare(queue=queue)
-    ch_w.basic_publish(
+def publish(ch: Channel):
+    ch.basic_publish(
         exchange="", routing_key=queue,
         body=json.dumps({"message": "hello World!", "author": "john"}),
     )
 
+
+@pytest.mark.asyncio
+async def test_all():
+    print()
+    await amqp.connect()
+    amqp.assert_queue(queue)
+    amqp.consume(queue, on_message)
+
+    new_ch = await amqp.create_channel()
+    publish(new_ch)
+
+    await asyncio.sleep(1)
     amqp.close()
-    await asyncio.sleep(3)
+    await asyncio.sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_consume():
+    print()
+    await amqp.connect()
+    amqp.assert_queue(queue)
+    amqp.consume(queue, on_message)
+    await asyncio.sleep(10)
+
+
+@pytest.mark.asyncio
+async def test_publish():
+    print()
+    conn, ch = await amqp.connect()
+    amqp.assert_queue(queue)
+    publish(ch)
+    publish(ch)
+    publish(ch)
+    publish(ch)
+    publish(ch)
+    await asyncio.sleep(1)
