35be32d53a7e8287c897b1e4801830e1f3e5fbc0
rwiv <zc798546@gmail.com>
2025-03-15 04:30:00 +0900

    feat: update venv_init script

--- scripts/venv_init.bat
+++ scripts/venv_init.bat
@@ -2,5 +2,4 @@ cd ..
 python -m venv .venv
 .venv\Scripts\pip.exe install -r requirements.txt
 .venv\Scripts\pip.exe install -r requirements-dev.txt
-.venv\Scripts\pip.exe install git+https://github.com/rwiv/pyutils.git@v0.1.3
 pause
