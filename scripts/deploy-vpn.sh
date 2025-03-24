c0b9b3e94acc3cb9d7ce7430ff87716f31880733
rwiv <zc798546@gmail.com>
2025-03-25 04:37:05 +0900

    feat: update vpn docker compose

--- scripts/deploy-vpn.sh
+++ scripts/deploy-vpn.sh
@@ -2,12 +2,16 @@
 
 cd ~/project/stdl
 
-sudo docker compose -f ./docker/docker-compose-server-vpn.yml --env-file ./secret/.env stop
-sudo docker compose -f ./docker/docker-compose-server-vpn.yml --env-file ./secret/.env rm -f
+sudo docker compose -f ./docker/docker-compose-server-vpn1.yml --env-file ./secret/.env stop
+sudo docker compose -f ./docker/docker-compose-server-vpn1.yml --env-file ./secret/.env rm -f
+
+sudo docker compose -f ./docker/docker-compose-server-vpn2.yml --env-file ./secret/.env stop
+sudo docker compose -f ./docker/docker-compose-server-vpn2.yml --env-file ./secret/.env rm -f
 
 git pull
 
-sudo docker compose -f ./docker/docker-compose-server-vpn.yml --env-file ./secret/.env up -d
+sudo docker compose -f ./docker/docker-compose-server-vpn1.yml --env-file ./secret/.env up -d
+sudo docker compose -f ./docker/docker-compose-server-vpn2.yml --env-file ./secret/.env up -d
 
 docker rmi harbor.rwiv.xyz/private/stdl:0.3.9
 sleep 3
