247970afc7ea29a3b751c9c7ce2bff19adbc5b4b
rwiv <zc798546@gmail.com>
2025-03-25 02:03:38 +0900

    feat: add vpn docker compose

---
+++ scripts/deploy-vpn.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd ~/project/stdl
+
+sudo docker compose -f ./docker/docker-compose-server-vpn.yml --env-file ./secret/.env stop
+sudo docker compose -f ./docker/docker-compose-server-vpn.yml --env-file ./secret/.env rm -f
+
+git pull
+
+sudo docker compose -f ./docker/docker-compose-server-vpn.yml --env-file ./secret/.env up -d
+
+docker rmi harbor.rwiv.xyz/private/stdl:0.3.9
+sleep 3
+docker logs stdl
