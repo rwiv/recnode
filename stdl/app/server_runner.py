b5ed9db7928f244ee150e1e2a3ea986d4851c65a
rwiv <zc798546@gmail.com>
2025-05-02 22:32:27 +0900

    feat: add server error handler

--- stdl/app/server_runner.py
+++ stdl/app/server_runner.py
@@ -1,14 +1,33 @@
 import logging
 
 import uvicorn
-from fastapi import FastAPI
-from pyutils import log
+from fastapi import FastAPI, Request, Response
+from pyutils import log, stacktrace
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import JSONResponse
 
 from .server_main_router import MainController
 from ..common.env import get_env
 from ..data.live import LiveStateService
 from ..data.redis import create_redis_client, RedisMap
 from ..recorder import RecordingScheduler, disable_streamlink_log
+
+
+async def handle_error(request: Request, call_next):
+    try:
+        response: Response = await call_next(request)
+        return response
+    except Exception as ex:
+        response = JSONResponse(
+            status_code=500,
+            content={
+                "message": str(ex),
+                "method": request.method,
+                "url": str(request.url),
+                "stacktrace": stacktrace(),
+            },
+        )
+        return response
 
 
 def run_server():
@@ -25,6 +44,7 @@ def run_server():
     main_controller = MainController(scheduler, live_state_service)
 
     app = FastAPI()
+    app.add_middleware(BaseHTTPMiddleware, dispatch=handle_error)
     app.include_router(main_controller.router)
 
     uvicorn.run(app, port=env.port, host="0.0.0.0", access_log=False)
