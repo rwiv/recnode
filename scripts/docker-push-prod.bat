8a9b86af2c7376251616983d87a56eb59d79bb0d
rwiv <zc798546@gmail.com>
2025-05-06 22:21:03 +0900

    deploy: add multi-platform push script

---
+++ scripts/docker-push-prod.bat
@@ -0,0 +1,9 @@
+cd ..
+set IMG=harbor.rwiv.xyz/private/stdl:0.6.0
+set DOCKERFILE=./docker/Dockerfile
+
+docker buildx build --platform linux/amd64,linux/arm64 -t %IMG% -f %DOCKERFILE% --push .
+
+docker stop buildx_buildkit_multi-arch-builder0
+docker rm buildx_buildkit_multi-arch-builder0
+pause
