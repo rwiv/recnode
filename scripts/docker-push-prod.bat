c15ffb1e4ee6457579c56337f20e264cad6b6ae8
rwiv <zc798546@gmail.com>
2025-05-15 23:57:01 +0900

    infra: upgrade version

--- scripts/docker-push-prod.bat
+++ scripts/docker-push-prod.bat
@@ -1,5 +1,5 @@
 cd ..
-set IMG=harbor.rwiv.xyz/private/stdl:0.6.3
+set IMG=harbor.rwiv.xyz/private/stdl:0.6.4
 set DOCKERFILE=./docker/Dockerfile
 
 docker buildx build --platform linux/amd64,linux/arm64 -t %IMG% -f %DOCKERFILE% --push .
